a05a057ac7ee7ab161f6c88c0bca9083ae7e361b
Tibor Meller <tibor.meller@gmail.com>
2024-03-05 08:43:58 +0100

    [#9] Adding tailwind to category listing page

--- gui/containers/MainContainer/MainContainer.tsx
+++ gui/containers/MainContainer/MainContainer.tsx
@@ -1,17 +1,47 @@
 import React from "react";
-import categories from "../../../json-cache/categories.json";
 
 import styles from "./styles.module.scss";
+import { Category } from "@core/models/category/category";
+
+const categories: Category[] = require("../../../json-cache/categories.json");
 
 export function MainContainer() {
     return (
         <div className={styles.mainContainer}>
-            <ul>
+            <ul role="list" className="divide-y divide-gray-100">
                 {categories.map((category) => {
-                    return <li key={category.id}>{category.name}</li>;
+                    return (
+                        <li key={category.id} className="flex justify-between gap-x-6 py-5">
+                            <div className="flex min-w-0 gap-x-4">
+                                <img
+                                    className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                                    src="{category.image}"
+                                    alt=""
+                                />
+                                <div className="min-w-0 flex-auto">
+                                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                                        {category.name}
+                                    </p>
+                                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                                        leslie.alexander@example.com
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                                <p className="text-sm leading-6 text-gray-900">
+                                    Co-Founder / CEO
+                                </p>
+                                <p className="mt-1 text-xs leading-5 text-gray-500">
+                                    Last seen{" "}
+                                    <time dateTime="2023-01-23T13:23Z">
+                                        3h ago
+                                    </time>
+                                </p>
+                            </div>
+                        </li>
+                    );
                 })}
             </ul>
         </div>
     );
 }
-
---
+++ gui/models/category/category.ts
@@ -0,0 +1,7 @@
+export interface Category {
+    id: string;
+    name: string;
+    im: string;
+    url: string;
+    subs: Category[];
+}
