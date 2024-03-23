af8a0ba38e12e7cb1301fef059d7bd76fb763573
fllSt4ck <fllst4ck@telekom.ip>
2024-03-23 19:21:38 +0100

    Introducing devcontainer infrastructure

---
+++ data-transplant/scripts/env-vars.sh
@@ -0,0 +1,5 @@
+# Fill up with the correct URLs
+export CATEGORY_URL=""
+export PRODUCT_SEARCH_URL=""
+export INVENTORY_URL=""
+export CLIENT_TOKEN=""
--- scripts/cli-url-env-vars.sh
+++
@@ -1,4 +0,0 @@
-# Fill up with the correct URLs
-export CATEGORY_URL=""
-export PRODUCT_SEARCH_URL=""
-export INVENTORY_URL=""
