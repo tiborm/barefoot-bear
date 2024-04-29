e93cd25c59a0b01f7757a08ee2f91c2b863258fe
Tibor Meller <tibor.meller@gmail.com>
2024-04-29 14:35:40 +0200

    Adding a test for id cleanup in categories

--- data-transplant/internal/data/transplant/category/category_test.go
+++ data-transplant/internal/data/transplant/category/category_test.go
@@ -7,6 +7,22 @@ import (
 	"github.com/tiborm/barefoot-bear/internal/model"
 )
 
+func TestGetCategories(t *testing.T) {
+	// Test data
+	url := "http://localhost:8080/categories"
+	outputDirectory := "test"
+	fileName := "categories.json"
+	forceFetch := false
+
+	// Test fetching categories
+	_, err := GetCategories(url, outputDirectory, fileName, forceFetch)
+
+	if err != nil {
+		t.Errorf("Failed to fetch categories: %s", err.Error())
+	}
+
+}
+
 func TestExtractingSubCategories(t *testing.T) {
 	// Test data
 	category := model.Category{
@@ -48,6 +64,22 @@ func TestExtractingSubCategories(t *testing.T) {
 	for _, ID := range []string{"1", "1.1", "1.2", "1.3.1", "1.4.1"} {
 		if !slices.Contains(*IDs, ID) {
 			t.Errorf("Failed to extract sub-category: %s", ID)
+		}
+	}
+}
+
+func TestCleaningUpIDs(t *testing.T) {
+	testIDs := []string{"ID1", "ID2", "ID3", "ID3", "ID3", "ID///4", "ID....5", "ID&^6"}
+
+	cleanedIDs := cleanUpIDs(testIDs)
+
+	if len(cleanedIDs) != 3 {
+		t.Errorf("Failed to remove duplicate IDs")
+	}
+
+	for i, ID := range(cleanedIDs) {
+		if cleanedIDs[i] != testIDs[i] {
+			t.Errorf("Failed to remove special characters from ID: %s", ID)
 		}
 	}
 }
